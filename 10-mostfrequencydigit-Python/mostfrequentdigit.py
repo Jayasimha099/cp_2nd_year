729e70d40b58aa62225c3d4969c6b3f77a8ece35
Jayasimha Reddy M <jayasimha1495@gmail.com>
2020-07-21 00:32:05 +0530

    Jul 21  00:32:04

--- 10-mostfrequencydigit-Python/mostfrequentdigit.py
+++ 10-mostfrequencydigit-Python/mostfrequentdigit.py
@@ -5,3 +5,26 @@
 def mostfrequentdigit(n):
 	# your code goes here
 	pass
+	n = abs(n)
+	if n == 0:
+		return 0
+	
+	dic = {}
+	count = 0
+	a = 0
+	while n!=0:
+		a = n%10
+		if a in dic.keys():
+			dic[a] += 1
+		else: 
+			dic[a] =1
+		n=n//10
+
+	for i in dic.keys():
+		if dic[i]>count:
+			count = dic[i]
+			a = i
+		elif dic[i] == count and i<a:
+			a =i
+	return a
+		
