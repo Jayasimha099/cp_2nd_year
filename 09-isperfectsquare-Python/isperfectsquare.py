11b9324ff9a873067db41cf8ac019b547b45c761
Jayasimha Reddy M <jayasimha1495@gmail.com>
2020-07-20 20:34:04 +0530

    Jul 20  20:34:03

--- 09-isperfectsquare-Python/isperfectsquare.py
+++ 09-isperfectsquare-Python/isperfectsquare.py
@@ -3,6 +3,11 @@
 # it is an int that is a perfect square (that is, if there exists an integer m such that
 # m**2 == n), and False otherwise. Do not crash on non-ints nor on negative ints.
 
+import math
 def isperfectsquare(n):
 	# your code goes here
 	pass
+
+	if type(n) != int:
+    		return False
+	return ((sqrt(n) - math.floor(sqrt(n))) == 0) if n>0 else False
