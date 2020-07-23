d7e4c46a6ae8fc85009bf144900f78d3d387e0a6
Jayasimha Reddy M <jayasimha1495@gmail.com>
2020-07-23 22:54:35 +0530

    Jul 23  22:54:35

--- 08-nth_additive_prime-Python/nth_additive_prime.py
+++ 08-nth_additive_prime-Python/nth_additive_prime.py
@@ -4,7 +4,33 @@
 # is also prime, so 113 is an Additive Prime. fun_nth_additive_prime(0) returns 2
 
 
-
-
+def check_prime(m):
+	if m<=1:
+			return False
+	for i in range(2,m):
+    		if m%i == 0:
+    				return False
+	return True
+    			
 def fun_nth_additive_prime(n):
-	return 1
+	if n == 0:
+    		return 2
+	m=3
+	p =0
+	q=[]
+	r =0
+
+	while len(l)!= n:
+			if check_prime(m):
+				r = list(str(m))
+				p = sum(map(int,r))
+				if check_prime(p):
+						q.append(m)
+			m += 1
+	return q[n-1]
+			
+			
+						
+    						
+
+	
